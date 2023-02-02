f0fe5ae1d02ab5472667ab55b2ee9286256f5bb8
lethargyst <ilushandr@gmail.com>
2023-02-02 22:47:53 +0500

    little fixes

--- src/precompiled.h
+++ src/precompiled.h
@@ -14,63 +14,15 @@ const float MAX_FPS = 60.0;
 const float MAX_PERIOD = 1.0 / MAX_FPS;
 
 const unsigned int MOVEMENT_SPREAD = 5;
-const float SPEED = 0.001f;
+const float SPEED = 0.003f;
 const float SOURCE_SIZE = 0.07f;
 const float ANT_SIZE = 0.005f;
 const float SHOUT_RANGE = 0.02f;
 const unsigned int TIME_TO_SHOUT = 1;
 
-const unsigned int MAX_OBJECTS_PER_NODE = 50;
+const unsigned int MAX_OBJECTS_PER_NODE = 100;
 const unsigned int MAX_DEPTH = 6;
 
 
 #endif
 
-// bool Ant::changeConditionBy(const Ant *other)
-// {
-//     float distSq = DistanceSq(this->pos_, other->pos_);
-
-//     if (this->motherID_ == 0) {
-//         this->motherID_ = other->motherID_;
-//         this->distanceToMother_ = other->distanceToMother_ + distSq;
-//         this->moveTo(other);
-//         this->isMovingToMother = true;
-//         this->changedStatus = true;
-//     }
-
-//     if (this->isMovingToMother) {
-//         if (other->motherID_ == this->motherID_ && this->distanceToMother_ - (other->distanceToMother_ + distSq) > epsilon) {
-//             this->distanceToMother_ = other->distanceToMother_ + distSq;
-//             this->moveTo(other);
-//             this->changedStatus = true;
-//         } else if (other->targetID_ == this->motherID_ && this->distanceToMother_ - (other->distanceToTarget_ + distSq) > epsilon) {
-//             this->distanceToMother_ = other->distanceToTarget_ + distSq;
-//             this->moveTo(other);
-//             this->changedStatus = true;
-//         }
-        
-//         if (this->distanceToTarget_ - (other->distanceToMother_ + distSq) > epsilon) {
-//             this->targetID_ = other->motherID_;
-//             this->distanceToTarget_ = other->distanceToMother_ + distSq;
-//             this->changedStatus = true;
-//         } 
-//     } else {
-//         if (other->motherID_ != this->motherID_ && this->distanceToTarget_ - (other->distanceToMother_ + distSq) > epsilon) {
-//             this->distanceToTarget_ = other->distanceToMother_ + distSq;
-//             this->moveTo(other);
-//             this->changedStatus = true;
-//         } else if (other->targetID_ != this->motherID_ && this->distanceToTarget_ - (other->distanceToTarget_ + distSq) > epsilon) {
-//             this->distanceToTarget_ = other->distanceToTarget_ + distSq;   
-//             this->moveTo(other);
-//             this->changedStatus = true;
-//         }
-
-//         if (this->distanceToMother_ - (other->distanceToTarget_ + distSq) > epsilon) {
-//             this->distanceToMother_ = other-> + distSq;
-//             this->changedStatus = true;
-//         } 
-//     } 
-
-//     if (changedStatus) this->timeToShout_ = TIME_TO_SHOUT;
-//     return changedStatus;
-// }
